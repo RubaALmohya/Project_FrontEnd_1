4b77df6af602f8d0bca719e7e4be13c2bf76a2a6
RubaALmohya <ruba12399@gmail.com>
2022-06-24 19:45:53 +0300

    Home page , Register page [Style has not been completed]

--- JS/script.js
+++ JS/script.js
@@ -19,4 +19,29 @@ function removeActiveClasses() {
 
 const mainTexthomeP = document.textContent("#mainText")
 
+function store(){
+
+    var name = document.getElementById('name');
+    var pw = document.getElementById('pw');
+
+    if(name.value.length == 0){
+        alert('Please fill in email');
+
+    }else if(pw.value.length == 0){
+        alert('Please fill in password');
+
+    }else if(name.value.length == 0 && pw.value.length == 0){
+        alert('Please fill in email and password');
+
+    }else if(pw.value.length > 8){
+        alert('Max of 8');
+    }else{
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
+        alert('Your account has been created');
+    }
+}
+
+
+
 
