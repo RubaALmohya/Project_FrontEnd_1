c8dba5b1c033007695d85daad2bf963bc9d25531
RubaALmohya <ruba12399@gmail.com>
2022-06-25 22:00:15 +0300

    Pages are initially completed

--- JS/script.js
+++ JS/script.js
@@ -21,27 +21,58 @@ const mainTexthomeP = document.textContent("#mainText")
 
 function store(){
 
-    var name = document.getElementById('name');
-    var pw = document.getElementById('pw');
+    let name = document.getElementById('name');
+    let email = document.getElementById('email')
+    let pw = document.getElementById('pw');
+    let radioMr = document.getElementById('radioMr');
+    let radioMs = document.getElementById('radioMs');
+
 
     if(name.value.length == 0){
-        alert('Please fill in email');
+        alert('Please fill in name');
+
+    }else if(email.value.length == 0){
+            alert('Please fill in email');
 
     }else if(pw.value.length == 0){
         alert('Please fill in password');
 
-    }else if(name.value.length == 0 && pw.value.length == 0){
-        alert('Please fill in email and password');
+    }else if(name.value.length == 0 && pw.value.length == 0 && email.value.length){
+        alert('Please fill in name , email and password');
 
     }else if(pw.value.length > 8){
         alert('Max of 8');
     }else{
+
         localStorage.setItem('name', name.value);
+        localStorage.setItem('email',email.value);
         localStorage.setItem('pw', pw.value);
+        // localStorage.setItem('radioMs',radioMs.value);
+        // localStorage.setItem('radioMr',radioMr.value);
         alert('Your account has been created');
     }
 }
 
 
+
+function check(){
+   
+    let storedName = localStorage.getItem('name');
+    let storedPw = localStorage.getItem('pw');
+
+    let userName = document.getElementById('userName');
+    let userPw = document.getElementById('userPw');
+
+    if(userName.value == storedName && userPw.value == storedPw){
+        // alert('You are logged in.')
+       location.href ='/HTML/index.html';
+     //let logButton = document.querySelector('#login_btn').innerHTML = "You are logged in."+window.location.href = 'HTML/index.html';
+     // location.href = '/HTML/index.html'
+        // let url = "/HTML/index.html"
+        // window.location(url)
+    }else{
+        alert('Error on login');
+    }
+}
 
 
